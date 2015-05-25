d58a6f15b184a63e858814e8ea65aef596423789
jukaraider <gallego.osvaldo@gmail.com>
2015-05-25 13:49:12 -0300

    FS levanta su configuracion

--- fileSystem/src/fileSystem.c
+++ fileSystem/src/fileSystem.c
@@ -1,21 +1,58 @@
-/*
- ============================================================================
- Name        : fileSystem.c
- Author      : 
- Version     :
- Copyright   : 
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
+#include "fileSystem.h"
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <commons/temporal.h>
+t_log* loggerFS;
 
-int main(void) {
-	char* tiempo = temporal_get_string_time();
-	puts(tiempo); /* prints  */
-	free(tiempo);
+char** vListaNodosMinima;
+u_int16_t iPuertoFS;
+
+int main(int argc, char *argv[]){
+
+	if (argc==1){
+		perror("No se puede iniciar el File System, falta indicar archivo de configuracion.");
+		exit(EXIT_FAILURE);
+	}
+
+	// INICIO - Carga de configuracion del personaje
+
+		cargarConfiguracion(argv[1]);
+
+	// FIN - Carga de configuracion del personaje
 
 	return EXIT_SUCCESS;
 }
+
+void cargarConfiguracion(char* pathArchiConf){
+
+	t_config* archivoConfig = config_create(pathArchiConf);
+	char* valueProperty;
+
+	if (config_has_property(archivoConfig, "PATH_LOG")) {
+		valueProperty = (char*) config_get_string_value(archivoConfig, "PATH_LOG");
+		loggerFS = log_create(valueProperty, "FileSystem", true, LOG_LEVEL_DEBUG);
+	}else {
+		loggerFS = log_create("logFileSystem.log", "FileSystem", true, LOG_LEVEL_DEBUG);
+	}
+	log_info(loggerFS, "Se inicializa el FS con el archivo %s", pathArchiConf);
+
+	if (!config_has_property(archivoConfig, "LISTA_NODOS")) {
+		log_error(loggerFS, "No se encontro la key 'LISTA_NODOS' en el archivo de configuracion");
+		exit(EXIT_FAILURE);
+	}
+	if (!config_has_property(archivoConfig, "PUERTO_LISTEN")) {
+		log_error(loggerFS, "No se encontro la key 'PUERTO_LISTEN' en el archivo de configuracion");
+		exit(EXIT_FAILURE);
+	}
+
+	valueProperty = config_get_string_value(archivoConfig, "LISTA_NODOS");
+	char* listaNodosMinima = (char*)malloc(sizeof(char)*strlen(valueProperty));
+	strcpy(listaNodosMinima, valueProperty);
+	vListaNodosMinima = string_get_string_as_array(listaNodosMinima);
+
+	strcpy(valueProperty, config_get_string_value(archivoConfig, "PUERTO_LISTEN"));
+	iPuertoFS = atoi(valueProperty);
+
+	log_info(loggerFS, "Se termino de cargar los datos de configuracion del FS");
+
+	config_destroy(archivoConfig);
+}
+
---
+++ fileSystem/src/fileSystem.h
@@ -0,0 +1,15 @@
+#ifndef FILESYSTEM_H_
+
+#define FILESYSTEM
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <commons/log.h>
+#include <commons/config.h>
+#include <commons/string.h>
+#include <commons/collections/list.h>
+
+void cargarConfiguracion(char* pathArchiConf);
+
+#endif /* FILESYSTEM_H_ */
