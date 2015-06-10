314c632b8a5bf6ec7379f4fbf87d443955e57395
alejandrozalazar <alejandrozalazar@gmail.com>
2015-06-09 23:04:31 -0300

    Se agrega mensaje de handshake de nodo a filesystem

--- commons/src/mensajeria/mensajes.h
+++ commons/src/mensajeria/mensajes.h
@@ -62,9 +62,13 @@ typedef enum {
 
 	/*Enviados desde el filesystem*/
 	FS_TO_MARTA_BLOQUES_ARCHIVO,
+	FS_TO_NODO_HANDSHAKE_OK,
 	FS_TO_NODO_GET_BLOQUE,
 	FS_TO_NODO_SET_BLOQUE,
 	FS_TO_NODO_GET_FILE_CONTENT,
+
+	/*Enviados desde el nodo*/
+	NODO_TO_FS_HANDSHAKE,
 
 	FIN
 } t_header;
