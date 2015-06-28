f0888982f3de9155ef640a06e0a1d45663eedccd
alejandrozalazar <alejandrozalazar@gmail.com>
2015-06-28 19:26:58 -0300

    Se agregan funciones de prueba

---
+++ fileSystem/src/testnodo.h
@@ -0,0 +1,13 @@
+/*
+ * testnodo.h
+ *
+ *  Created on: Jun 28, 2015
+ *      Author: Alejandro Zalazar
+ */
+
+#ifndef SRC_TESTNODO_H_
+#define SRC_TESTNODO_H_
+
+#include "fileSystem.h"
+
+#endif /* SRC_TESTNODO_H_ */
