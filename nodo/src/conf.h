d338675fd45ceaa7d7c6d689ca537bf56191c2fd
alejandrozalazar <alejandrozalazar@gmail.com>
2015-07-18 20:33:14 -0300

    Bugfix en struct de configuracion

--- nodo/src/conf.h
+++ nodo/src/conf.h
@@ -19,13 +19,13 @@
 #include "utils.h"
 
 typedef struct conf_nodo {
-	char *NOMBRE_NODO;
+	char* NOMBRE_NODO;
 	int PUERTO_FS; //	3000	Puerto donde el FileSystem está escuchando solicitudes de nuevas conexiones
-	char *IP_FS; //	x.y.z.w	IP de la computadora donde está ejecutándose el FileSystem
+	char* IP_FS; //	x.y.z.w	IP de la computadora donde está ejecutándose el FileSystem
 	char* ARCHIVO_BIN; // 	data.bin	Nombre del archivo que contiene los bloques de datos
 	char* DIR_TEMP; //	/tmp	Directorio donde se van a almacenar los archivos temporales
 	bool NODO_NUEVO; //	SI/NO	Un nodo nuevo es aquel que nunca formó parte del cluster. Este valor debe estar en SI la primera vez que un nodo ingresa al sistema y luego cambiar a NO.
-	char *IP_NODO; //	a.b.c.d.	IP de la computadora donde está ejecutándose este proceso nodo. (puede obtenerse del sistema)
+	char* IP_NODO; //	a.b.c.d.	IP de la computadora donde está ejecutándose este proceso nodo. (puede obtenerse del sistema)
 	int PUERTO_NODO; //	6000	Puerto en el cual este proceso Nodo espera recibir conexiones nuevas.
 	int BLOCK_SIZE_IN_BYTES; // 20k Tamano de bloque
 	char* TAMANIO_ESPACIO_DATOS_EN_BYTES; // 20k Tamano de bloque
