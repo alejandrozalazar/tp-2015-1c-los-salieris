f361a4ded70ac9e60d0a1fda78daeee1d13bb263
mariano85 <mariano.umbides@gmail.com>
2015-07-18 13:58:19 -0300

    - reestructuracion marta

---
+++ marta/Debug/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../escucha.c \
+../marta.c 
+
+OBJS += \
+./escucha.o \
+./marta.o 
+
+C_DEPS += \
+./escucha.d \
+./marta.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: GCC C Compiler'
+	gcc -I"/home/utnso/workspace-1c2015/commons" -O0 -g3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
